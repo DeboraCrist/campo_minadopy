6a73948bbaed6588e0900c98163576ad84ff6ab1
DeboraCrist <deboraccrist70@gmail.com>
2023-10-09 22:17:06 -0300

    colocando a opção de sair e reiniciar no main

--- main.py
+++ main.py
@@ -11,42 +11,46 @@ def main():
         else:
             print("Nível de dificuldade inválido. Escolha 1, 2 ou 3.")
 
-    jogo = CampoMinado(nivel)
+    while True: 
+        jogo = CampoMinado(nivel)
 
-    while True:
-        for row in jogo.tabuleiro:
-            print(" ".join(row))
+        while True:  
+            for row in jogo.tabuleiro:
+                print(" ".join(row))
 
-        acao = input("Escolha uma ação (D para descobrir, B para colocar bandeira, R para remover bandeira, Q para sair): ").upper()
+            acao = input("Escolha uma ação (D para descobrir, B para colocar bandeira, R para remover bandeira, Q para sair ou N para reiniciar): ").upper()
 
-        if acao == 'Q':
-            print("Saindo do jogo.")
-            break
-        elif acao == 'D':
-            x = int(input("Digite a coordenada X: "))
-            y = int(input("Digite a coordenada Y: "))
-            jogo.descobrir_zona(x, y)
-            if jogo.jogo_encerrado:
-                print("Você perdeu!")
-                jogo.revelar_bombas()
-                for row in jogo.tabuleiro:
-                    print(" ".join(row))
-                break
-            elif jogo.jogo_vencido:
-                print("Parabéns! Você venceu!")
-                for row in jogo.tabuleiro:
-                    print(" ".join(row))
-                break
-        elif acao == 'B':
-            x = int(input("Digite a coordenada X para colocar a bandeira: "))
-            y = int(input("Digite a coordenada Y para colocar a bandeira: "))
-            jogo.colocar_bandeira(x, y)
-        elif acao == 'R':
-            x = int(input("Digite a coordenada X para remover a bandeira: "))
-            y = int(input("Digite a coordenada Y para remover a bandeira: "))
-            jogo.remover_bandeira(x, y)
-        else:
-            print("Ação inválida. Escolha D para descobrir, B para colocar bandeira, R para remover bandeira ou Q para sair.")
+            if acao == 'Q':
+                print("Saindo do jogo.")
+                return  
+            elif acao == 'N':
+                print("Reiniciando o jogo.")
+                break  
+            elif acao == 'D':
+                x = int(input("Digite a coordenada X: "))
+                y = int(input("Digite a coordenada Y: "))
+                jogo.descobrir_zona(x, y)
+                if jogo.jogo_encerrado:
+                    print("Você perdeu!")
+                    jogo.revelar_bombas()
+                    for row in jogo.tabuleiro:
+                        print(" ".join(row))
+                    break
+                elif jogo.jogo_vencido:
+                    print("Parabéns! Você venceu!")
+                    for row in jogo.tabuleiro:
+                        print(" ".join(row))
+                    break
+            elif acao == 'B':
+                x = int(input("Digite a coordenada X para colocar a bandeira: "))
+                y = int(input("Digite a coordenada Y para colocar a bandeira: "))
+                jogo.colocar_bandeira(x, y)
+            elif acao == 'R':
+                x = int(input("Digite a coordenada X para remover a bandeira: "))
+                y = int(input("Digite a coordenada Y para remover a bandeira: "))
+                jogo.remover_bandeira(x, y)
+            else:
+                print("Ação inválida. Escolha D para descobrir, B para colocar bandeira, R para remover bandeira, N para reiniciar ou Q para sair.")
 
 if __name__ == "__main__":
     main()
